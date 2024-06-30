0a20d817e39de1d2091528c681dc2fa8f6642987
Jesse <code@seqmanagement.com>
2024-06-29 19:00:28 -0700

    updated formatDate to handle both date and string objects

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,18 +1,33 @@
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 export function formatDate(date) {
+  if (date instanceof Date) return formatDateObj(date);
+  return formatDateStr(date);
+}
+function formatDateObj(date) {
+  const formatter = new Intl.DateTimeFormat("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "2-digit",
+  });
+
+  return formatter.format(date).replace(",", "-");
+}
+
+function formatDateStr(date) {
   const parts = date.split(/[- :]/); // Split by hyphens, spaces, and colons
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  return `${parts[2]}-${months[parseInt(parts[1]) - 1]}-${parts[0].slice(-2)}`;
+  return `${parts[2]}-${MONTHS[parseInt(parts[1]) - 1]}-${parts[0].slice(-2)}`;
 }
